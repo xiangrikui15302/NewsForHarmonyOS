9c352f2f191f80b4f8821013f0fae5cdb9d647d5
xiangrikui15302 <767801875@qq.com>
2024-03-22 10:22:24 +0800

    拷贝项目

---
+++ shopServer/models/newData.js
@@ -0,0 +1,221 @@
+exports.data = [
+
+    {
+        id: 2,
+        title: "中部地区崛起座谈会再次召开 习近平作出哪些部署？",
+        type:1,
+        images: [
+            "/images/faca41195272.jpg",
+            "/images/a9f88d706914.jpg",
+            "/images/jpJmLxG6urQ.jpg",
+        ],
+        author: "央视新闻",
+        contentUrl:'https://mbd.baidu.com/newspage/data/landingshare?context=%7B%22nid%22%3A%22news_9939762621688781296%22%2C%22sourceFrom%22%3A%22wise_feedlist%22%7D&rec_src=52'
+
+    },
+    {
+        id: 3,
+        title: "习近平向印尼当选总统普拉普拉得的致贺电",
+        type:1,
+        images: [
+            "/images/faca41195272.jpg",
+            "/images/a9f88d706914.jpg",
+            "/images/jpJmLxG6urQ.jpg",
+        ],
+        author: "新华网",
+        contentUrl:'https://mbd.baidu.com/newspage/data/landingshare?context=%7B%22nid%22%3A%22news_9939762621688781296%22%2C%22sourceFrom%22%3A%22wise_feedlist%22%7D&rec_src=52'
+
+    },
+    {
+        id: 4,
+        title: "科技助农 跑出春宫加速度",
+        type:1,
+        images: [
+            "/images/faca41195272.jpg",
+            "/images/a9f88d706914.jpg",
+            "/images/jpJmLxG6urQ.jpg",
+        ],
+        author: "新华网",
+        contentUrl:'https://mbd.baidu.com/newspage/data/landingshare?context=%7B%22nid%22%3A%22news_9939762621688781296%22%2C%22sourceFrom%22%3A%22wise_feedlist%22%7D&rec_src=52'
+
+    },
+    {
+        id: 5,
+        title: "物价气温回升 暖意之中有暖意",
+        type:1,
+        images: [
+            "/images/faca41195272.jpg",
+            "/images/a9f88d706914.jpg",
+            "/images/jpJmLxG6urQ.jpg",
+        ],
+        author: "新华网",
+        contentUrl:'https://mbd.baidu.com/newspage/data/landingshare?context=%7B%22nid%22%3A%22news_9939762621688781296%22%2C%22sourceFrom%22%3A%22wise_feedlist%22%7D&rec_src=52'
+
+    },
+    {
+        id: 6,
+        title: "中外嘉宾公话民主：全人类共同价值",
+        type:1,
+        images: [
+            "/images/faca41195272.jpg",
+            "/images/a9f88d706914.jpg",
+            "/images/jpJmLxG6urQ.jpg",
+        ],
+        author: "新华网",
+        contentUrl:'https://mbd.baidu.com/newspage/data/landingshare?context=%7B%22nid%22%3A%22news_9939762621688781296%22%2C%22sourceFrom%22%3A%22wise_feedlist%22%7D&rec_src=52'
+
+    },
+    {
+        id: 7,
+        title: "马斯克晒硬汉照 打沙袋表情凶狠 法媒想起赤膊骑马的普京",
+        type:3,
+        images: [
+            "/images/faca41195272.jpg",
+            "/images/a9f88d706914.jpg",
+            "/images/jpJmLxG6urQ.jpg",
+        ],
+        author: "环球网国际",
+        contentUrl:'https://mbd.baidu.com/newspage/data/landingshare?context=%7B%22nid%22%3A%22news_9890150548364180775%22%2C%22sourceFrom%22%3A%22wise_feedlist%22%7D'
+
+    },
+    {
+        id: 8,
+        title: "19岁网红被人刺伤住进ICU 家属凑款80万元医疗费",
+        type:2,
+        images: [
+            "/images/a9f88d706914.jpg"
+        ],
+        author: "环球网国际",
+        contentUrl:'https://mbd.baidu.com/newspage/data/landingshare?context=%7B%22nid%22%3A%22news_9890150548364180775%22%2C%22sourceFrom%22%3A%22wise_feedlist%22%7D'
+
+    },
+    {
+        id: 9,
+        title: "75后女副市长丁伟，主动向组织交代问题",
+        type:4,
+        images: [
+            "/images/faca41195272.jpg"
+        ],
+        author: "每日经济新闻",
+
+    },
+    {
+        id: 10,
+        title: "小伙银行取钱，结果柜员是老婆，夫妻俩对话笑喷了！爆笑取钱场面",
+        type:5,
+        images: [
+            "/images/faca41195272.jpg"
+        ],
+        author: "每日经济新闻",
+
+    },
+    {
+        id: 2,
+        title: "中部地区崛起座谈会再次召开 习近平作出哪些部署？",
+        type:1,
+        images: [
+            "/images/faca41195272.jpg",
+            "/images/a9f88d706914.jpg",
+            "/images/jpJmLxG6urQ.jpg",
+        ],
+        author: "央视新闻",
+        contentUrl:'https://mbd.baidu.com/newspage/data/landingshare?context=%7B%22nid%22%3A%22news_9939762621688781296%22%2C%22sourceFrom%22%3A%22wise_feedlist%22%7D&rec_src=52'
+
+    },
+    {
+        id: 3,
+        title: "习近平向印尼当选总统普拉普拉得的致贺电",
+        type:1,
+        images: [
+            "/images/faca41195272.jpg",
+            "/images/a9f88d706914.jpg",
+            "/images/jpJmLxG6urQ.jpg",
+        ],
+        author: "新华网",
+        contentUrl:'https://mbd.baidu.com/newspage/data/landingshare?context=%7B%22nid%22%3A%22news_9939762621688781296%22%2C%22sourceFrom%22%3A%22wise_feedlist%22%7D&rec_src=52'
+
+    },
+    {
+        id: 4,
+        title: "科技助农 跑出春宫加速度",
+        type:1,
+        images: [
+            "/images/faca41195272.jpg",
+            "/images/a9f88d706914.jpg",
+            "/images/jpJmLxG6urQ.jpg",
+        ],
+        author: "新华网",
+        contentUrl:'https://mbd.baidu.com/newspage/data/landingshare?context=%7B%22nid%22%3A%22news_9939762621688781296%22%2C%22sourceFrom%22%3A%22wise_feedlist%22%7D&rec_src=52'
+
+    },
+    {
+        id: 5,
+        title: "物价气温回升 暖意之中有暖意",
+        type:1,
+        images: [
+            "/images/faca41195272.jpg",
+            "/images/a9f88d706914.jpg",
+            "/images/jpJmLxG6urQ.jpg",
+        ],
+        author: "新华网",
+        contentUrl:'https://mbd.baidu.com/newspage/data/landingshare?context=%7B%22nid%22%3A%22news_9939762621688781296%22%2C%22sourceFrom%22%3A%22wise_feedlist%22%7D&rec_src=52'
+
+    },
+    {
+        id: 6,
+        title: "中外嘉宾公话民主：全人类共同价值",
+        type:1,
+        images: [
+            "/images/faca41195272.jpg",
+            "/images/a9f88d706914.jpg",
+            "/images/jpJmLxG6urQ.jpg",
+        ],
+        author: "新华网",
+        contentUrl:'https://mbd.baidu.com/newspage/data/landingshare?context=%7B%22nid%22%3A%22news_9939762621688781296%22%2C%22sourceFrom%22%3A%22wise_feedlist%22%7D&rec_src=52'
+
+    },
+    {
+        id: 7,
+        title: "马斯克晒硬汉照 打沙袋表情凶狠 法媒想起赤膊骑马的普京",
+        type:3,
+        images: [
+            "/images/faca41195272.jpg",
+            "/images/a9f88d706914.jpg",
+            "/images/jpJmLxG6urQ.jpg",
+        ],
+        author: "环球网国际",
+        contentUrl:'https://mbd.baidu.com/newspage/data/landingshare?context=%7B%22nid%22%3A%22news_9890150548364180775%22%2C%22sourceFrom%22%3A%22wise_feedlist%22%7D'
+
+    },
+    {
+        id: 8,
+        title: "19岁网红被人刺伤住进ICU 家属凑款80万元医疗费",
+        type:2,
+        images: [
+            "/images/a9f88d706914.jpg"
+        ],
+        author: "环球网国际",
+        contentUrl:'https://mbd.baidu.com/newspage/data/landingshare?context=%7B%22nid%22%3A%22news_9890150548364180775%22%2C%22sourceFrom%22%3A%22wise_feedlist%22%7D'
+
+    },
+    {
+        id: 9,
+        title: "75后女副市长丁伟，主动向组织交代问题",
+        type:4,
+        images: [
+            "/images/faca41195272.jpg"
+        ],
+        author: "每日经济新闻",
+
+    },
+    {
+        id: 10,
+        title: "小伙银行取钱，结果柜员是老婆，夫妻俩对话笑喷了！爆笑取钱场面",
+        type:5,
+        images: [
+            "/images/faca41195272.jpg"
+        ],
+        author: "每日经济新闻",
+
+    }
+]
